e3350bf3309aba41def5f3e687bf72ac24313c50
sterling <sterlingsrq@gmail.com>
2015-03-05 22:56:43 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express'),
+    request = require('request'),
+    cheerio = require('cheerio'),
+    app     = express();
+
+
+/**
+ * Gets the Nat Geo photo of the day in JSON format with the img src, img alt, and credit in HTML.
+ */
+app.get('/api/dailyphoto', function(req, res){
+    url = 'http://photography.nationalgeographic.com/photography/photo-of-the-day/';
+
+    request(url, function(err, response, html){
+        if (err) throw err;
+        var $ = cheerio.load(html);
+
+        res.send({ 
+            "src": $("div.primary_photo img").attr("src"),
+            "alt": $("div.primary_photo img").attr("alt"),
+            "credit": $("div#caption p.credit").html()
+        });
+    });
+});
+
+
+app.listen('8080')
+exports = module.exports = app;
